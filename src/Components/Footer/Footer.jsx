e263e8a3936e258d2509576e408e624657b679da
Liza Tokar <lizatokarr@gmail.com>
2024-05-07 21:41:18 +0300

    the second commit

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -10,7 +10,7 @@ class Footer extends React.Component {
         return (
             <div className="footer">
                 <a href="tel:+1 311-395-1395">+1 311-395-1395</a>
-                <p>4538 Fairfax Ave Culver City, C 89620</p>
+                <p>4538 Fairfax Ave Culver City, CA 89620</p>
                 <p>US Segways © 2024</p>
             </div>
         );
